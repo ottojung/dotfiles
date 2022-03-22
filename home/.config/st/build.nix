e9772db9d2c7c38240197eec9fda3ee6e7dd4157
Otto Jung <otto.jung@kakao.com>
2022-03-22 08:49:30 +0100

    remove st

--- home/.config/st/build.nix
+++
@@ -1,6 +0,0 @@
-with import <nixpkgs> {}; {
-	stEnv = stdenv.mkDerivation {
-		name = "stEnv";
-		buildInputs = [ gnutar gzip gnumake gcc binutils-unwrapped coreutils gawk gnused gnugrep patchelf findutils pkg-config xorg.libX11 xorg.libXft ];
-	};
-}
