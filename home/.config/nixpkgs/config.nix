0c0ae82b833be6437190ce2076521f3bec2f94ed
Otto Jung <otto.jung@vauplace.com>
2022-10-08 20:37:10 +0200

    nix: delete telegram

--- home/.config/nixpkgs/config.nix
+++ home/.config/nixpkgs/config.nix
@@ -124,7 +124,7 @@ paths =
 	###########
 	### big apps
 	audacity # audio editor
-	unstable.tdesktop # Telegram desktop client
+	# unstable.tdesktop # Telegram desktop client
 	# virtualbox # virtual machine (some parts are propriatary)
 	virt-manager # virtual machine helper
 	unstable.ungoogled-chromium # chromium without google services
