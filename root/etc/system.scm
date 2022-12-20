31463b06121df3c346f13372b1fbbd20f60ee09b
Otto Jung <otto.jung@vauplace.com>
2022-12-20 11:49:10 +0100

    guixsd: improve code readability

--- root/etc/system.scm
+++ root/etc/system.scm
@@ -7,7 +7,6 @@
 (use-modules ((guix packages) #:select (package-name)))
 (use-modules (nongnu packages linux)) ;; propriatary channels
 (use-modules (srfi srfi-1))
-;; (use-modules (ice-9 pretty-print))
 
 (use-service-modules
   cups
@@ -16,6 +15,64 @@
   virtualization
   ssh
   xorg)
+
+(define (main)
+  (operating-system
+   ;; propriatary kernel + firmware
+   (kernel linux)
+   (firmware (list linux-firmware))
+
+   (locale "en_US.UTF-8")
+   (timezone "Europe/Warsaw")
+   (keyboard-layout my-console-keyboard)
+
+   (host-name "pc")
+
+   ;; The list of user accounts ('root' is implicit).
+   (users (cons* (user-account
+                  (name "user1")
+                  (comment "user1")
+                  (group "users")
+                  (home-directory "/home/user1")
+                  (supplementary-groups
+                   '("wheel" "netdev" "audio" "video" "libvirt" "lp" "tty" "input" "kvm")))
+                 %base-user-accounts))
+
+   (packages
+    (add-my-packages
+     (remove-sudo-package
+      %base-packages)))
+
+   (setuid-programs
+    (remove-sudo-setuid-program
+     %setuid-programs))
+
+   (services
+    (add-my-desktop-services %desktop-services))
+
+   (bootloader (bootloader-configuration
+                (bootloader grub-efi-bootloader)
+                (targets (list "/boot/efi"))
+                (keyboard-layout keyboard-layout)))
+   (mapped-devices (list (mapped-device
+                          (source (uuid
+                                   "8b37a5aa-dd43-4f9c-901a-8e488ae6e701"))
+                          (target "cryptroot")
+                          (type luks-device-mapping))))
+
+   ;; The list of file systems that get "mounted".  The unique
+   ;; file system identifiers there ("UUIDs") can be obtained
+   ;; by running 'blkid' in a terminal.
+   (file-systems (cons* (file-system
+                         (mount-point "/boot/efi")
+                         (device (uuid "4ED6-B9A2"
+                                       'fat32))
+                         (type "vfat"))
+                        (file-system
+                         (mount-point "/")
+                         (device "/dev/mapper/cryptroot")
+                         (type "ext4")
+                         (dependencies mapped-devices)) %base-file-systems))))
 
 (define (set-in-alist alist . key-values)
   "Removes relevant keys and sets the values back."
@@ -67,8 +124,11 @@
 (define (add-libvirt-services services)
   (cons* my-libvirt-service my-virtlog-service services))
 
+(define (add-gnome-service packages)
+  (cons (service gnome-desktop-service-type) packages))
+
 (define add-my-desktop-services
-  (compose add-libvirt-services set-display-manager modify-special-files))
+  (compose add-gnome-service add-libvirt-services set-display-manager modify-special-files))
 
 (define my-system-package-names
   '("dash"
@@ -120,61 +180,4 @@
 (define (remove-sudo-setuid-program progs)
   (filter (negate sudo-setuid-program?) progs))
 
-(operating-system
-  ;; propriatary kernel + firmware
-  (kernel linux)
-  (firmware (list linux-firmware))
-
-  (locale "en_US.UTF-8")
-  (timezone "Europe/Warsaw")
-  (keyboard-layout my-console-keyboard)
-
-  (host-name "pc")
-
-  ;; The list of user accounts ('root' is implicit).
-  (users (cons* (user-account
-                  (name "user1")
-                  (comment "user1")
-                  (group "users")
-                  (home-directory "/home/user1")
-                  (supplementary-groups
-                   '("wheel" "netdev" "audio" "video" "libvirt" "lp" "tty" "input" "kvm")))
-                %base-user-accounts))
-
-  (packages
-   (add-my-packages
-    (remove-sudo-package
-     %base-packages)))
-
-  (setuid-programs
-   (remove-sudo-setuid-program
-    %setuid-programs))
-
-  (services
-   (cons
-    (service gnome-desktop-service-type)
-    (add-my-desktop-services %desktop-services)))
-
-  (bootloader (bootloader-configuration
-               (bootloader grub-efi-bootloader)
-               (targets (list "/boot/efi"))
-               (keyboard-layout keyboard-layout)))
-  (mapped-devices (list (mapped-device
-                         (source (uuid
-                                  "8b37a5aa-dd43-4f9c-901a-8e488ae6e701"))
-                         (target "cryptroot")
-                         (type luks-device-mapping))))
-
-  ;; The list of file systems that get "mounted".  The unique
-  ;; file system identifiers there ("UUIDs") can be obtained
-  ;; by running 'blkid' in a terminal.
-  (file-systems (cons* (file-system
-                        (mount-point "/boot/efi")
-                        (device (uuid "4ED6-B9A2"
-                                      'fat32))
-                        (type "vfat"))
-                       (file-system
-                        (mount-point "/")
-                        (device "/dev/mapper/cryptroot")
-                        (type "ext4")
-                        (dependencies mapped-devices)) %base-file-systems)))
+(main)
