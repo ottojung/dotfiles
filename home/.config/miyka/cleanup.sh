a4f12d32a32d43144abc368fef968a46fc6404aa
Otto Jung <otto.jung@vauplace.com>
2024-05-26 15:42:04 -0700

    fix cleanup.sh [2]

--- home/.config/miyka/cleanup.sh
+++ home/.config/miyka/cleanup.sh
@@ -6,25 +6,27 @@ firefox_cleanup() {
 	rm -r -f -v 'favicons.'*
 	rm -r -f -v 'gmp-'*
 	rm -r -f -v *safebrowsing*
+	return 0
 }
 
 chromium_cleanup() {
 	echo "Cleaning up chromium dir..." 1>&2
 	find -iname '*cache*' -not -path "./Default/Extensions/*" -exec rm -rf -v {} \;
+	return 0
 }
 
 cd -- "$MIYKA_REPO_HOME"
 
-cd -- ".config/chromium" && chromium_cleanup
+cd -- ".config/chromium" && chromium_cleanup && cd -
 
+cd -- "$MIYKA_REPO_HOME"
 if test -d ".mozilla"
 then
 	cd ".mozilla"
 	find -maxdepth 5 -type f -iname prefs.js | while IFS= read -r FILE
 	do
 		DIR="$(dirname "$FILE")"
-		cd -- "$DIR" && firefox_cleanup
-		cd -
+		cd -- "$DIR" && firefox_cleanup && cd -
 	done
 fi
 
