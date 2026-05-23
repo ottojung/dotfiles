b5eefe72a4b08503885cab5011b1296da8304114
Otto Jung <otto.jung@vauplace.com>
2026-05-22 22:11:56 -0700

    remove awesome wm

--- home/.config/guix/config.scm
+++ home/.config/guix/config.scm
@@ -128,7 +128,7 @@
   ;; slock ;; simple screen locking ;; broken? Says ~"cannot disable OOM"~ as if I wanted that.
   slim ;; provides `slimlock' screen locker
   xmessage ;; for displaying user messages
-  awesome ;; the awesome window manager
+  # awesome ;; the awesome window manager
   xdg-user-dirs ;; for xdg-user-dir command that is used by various programs
   qtile ;; the qtile window manager
   )
